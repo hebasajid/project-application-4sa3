43afd6798c77292953a2d33e94acb355974fe4f0
Heba Sajid <hsajid35467@gmail.com>
2026-02-26 21:00:22 -0700

    created a database connection - uri not added. and entered test sample resource upon initalizing

---
+++ db-test.js
@@ -0,0 +1,28 @@
+const {MongoClient} = require('mongodb');
+
+const uri =    //connection string to mongodb
+const client= new MongoClient(uri);
+
+async function run() { 
+    try {
+        await client.connect();
+        console.log('Connected to the Education Cloud Database!');
+        const database = client.db('education_cloud');
+        const inventory = database.collection('inventory');
+
+        //testing - inserting a sample resource:
+        const doc = {
+            name: 'Alphabet Flashcards for Kids',
+            type: 'Digital PDF',
+            price: 6.00
+        };
+        const result = await inventory.insertOne(doc);
+        console.log(`Successfully added test product with ID: ${result.insertedId}`);
+
+        } catch (error) {
+         console.error("Database connection failed:", error);
+       } finally {
+         await client.close();
+  }
+}
+run()
