d38cea71b4d992d6a3c2a7c13721da4fed7c2745
Heba Sajid <hsajid35467@gmail.com>
2026-03-12 17:19:56 -0600

    using factory pattern to make sure the controller doesnt create objects manually.

--- patterns/ProductFactory.js
+++ patterns/ProductFactory.js
@@ -1,1 +1,10 @@
 const Product = require('../models/Product');
+
+class ProductFactory {
+    static createProduct(id, name, price, description) {
+        console.log(`[Factory] Creating product object for: ${name}`);
+        return new Product(id, name, price, description);
+    }
+}
+
+module.exports = ProductFactory;
