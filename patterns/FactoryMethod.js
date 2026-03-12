588af02c7b22df2affbad15834c79f822d452293
Heba Sajid <hsajid35467@gmail.com>
2026-03-12 17:27:13 -0600

    names changed of files - mismatch of class names and diagrams

--- patterns/FactoryMethod.js
+++ patterns/FactoryMethod.js
@@ -1,10 +1,10 @@
 const Product = require('../models/Product');
 
-class ProductFactory {
+class FactoryMethod {
     static createProduct(id, name, price, description) {
         console.log(`[Factory] Creating product object for: ${name}`);
         return new Product(id, name, price, description);
     }
 }
 
-module.exports = ProductFactory;
+module.exports = FactoryMethod;
--- services/MongoService.js
+++ services/MongoService.js
@@ -5,3 +5,11 @@ class MongoService {
     constructor() {
         this.connectionString = config.mongoURI; 
     }
+
+async logTransaction(data) {
+        console.log(`[MongoDB] Logging transaction for product ${data.productId}...`);
+        return { logId: "LOG_" + Math.random().toString(36).substr(2, 9), ...data };
+    }
+}
+
+module.exports = MongoService;
