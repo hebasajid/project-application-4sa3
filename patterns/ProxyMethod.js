216b0e4f2bc3b5a4f624ce742265318e5a0d7bd2
Heba Sajid <hsajid35467@gmail.com>
2026-03-12 17:30:20 -0600

    initializing the consrtuctor for the proxymethod. for using stripe and mongoservce

--- patterns/ProxyMethod.js
+++ patterns/ProxyMethod.js
@@ -1,1 +1,12 @@
 //using Stripe as the payment gateway
+class ProxyMethod {
+
+    constructor() {
+        this.realStripe = new StripeService();
+        this.db = new MongoService();
+    }
+
+   
+}
+
+module.exports = PaymentProxy;
