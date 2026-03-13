9826fa10475f9864f23084ba59cc0a38bc061450
Heba Sajid <hsajid35467@gmail.com>
2026-03-13 01:05:31 -0600

    adding log updates based on success or failure of payment

--- patterns/ProxyMethod.js
+++ patterns/ProxyMethod.js
@@ -9,7 +9,31 @@ class ProxyMethod {
         this.db = new MongoService();
     }
 
+    async process(user, product) {
+        // traceability: Log start
+        const log = await this.db.logTransaction({
+            user: user.name,
+            productId: product.id,
+            amount: product.price,
+            status: "initiated"
+        });
    
+        console.log(`[Proxy] Traceability log created: ${log.logId}`);
+
+    try {
+            // forwarding to the  Stripe service
+            const stripeResponse = await this.realStripe.charge(product.price);
+            
+            // log updates on success
+            console.log(`[Proxy] Success Stripe ID: ${stripeResponse.id}`);
+            return { success: true, stripeId: stripeResponse.id };
+
+        } catch (error) {
+            // log updates on failure
+            console.log(`[Proxy] Payment failed. Log updated.`);
+            throw error;
+        }
+    }
 }
 
 module.exports = PaymentProxy;
