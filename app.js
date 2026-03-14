549b9c86b8dc52ee03d99ee3f220fba11235e802
Heba Sajid <hsajid35467@gmail.com>
2026-03-14 16:34:48 -0600

    using try catch block to first create a product with the factory methof and using th eproxy to process the payment + showing stripe transaction ID

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const FactoryMethod = require('./patterns/FactoryMethod');
 const ProxyMethod = require('./patterns/ProxyMethod');
 
 async function main() {
-    // using the factory to create a product
+    
+// using the factory to create a product
+
+try{
     const book = ProductFactory.createProduct(
         "101", //id
         "English Flashcards for Kids",  //name
@@ -11,13 +14,19 @@ async function main() {
 
     //proxy to process payment and log transactions:
     const paymentGate = new PaymentProxy();
+
+    console.log("Processing payment for:", book.name);
+
+    const result = await paymentGate.process({ name: "Student_User" }, book);
+        
+        console.log("--- Purchase Complete!! ---");
+        console.log("Stripe Transaction ID:", result.stripeId);
     
-    try {
-        const result = await paymentGate.process({ name: "Student_User" }, book);
-        console.log("Purchase Complete!!");
     } catch (err) {
-        console.error("Purchase Error:", err.message);
+        console.error("--- Purchase Error ---");
+        console.error("Message:", err.message);
     }
+
 }
 
 main();
