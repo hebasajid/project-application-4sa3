8ce20287ce6bba11cfbda1ee1b0f1682bfa8685f
Heba Sajid <hsajid35467@gmail.com>
2026-03-14 16:19:09 -0600

    creating the api call to send the data to stripe

--- services/StripeService.js
+++ services/StripeService.js
@@ -7,6 +7,17 @@ class StripeService {
     async charge(amount, productName) { //method to charge a payment
         console.log(`[Stripe API] Connecting to Stripe to process: ${productName}...`);
     
+        try {
+            // API call - sendign data to stripe
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: Math.round(amount * 100), //converting dollars to cents for stripe
+                currency: 'usd',
+                payment_method: 'pm_card_visa', // test card by stripe
+                confirm: true,
+                description: `M3 Submission Test: ${productName}`,
+                // allows  to run this in CLI without a browser
+                automatic_payment_methods: { enabled: true, allow_redirects: 'never' }
+            });
    
 }
 
